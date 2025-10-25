0c4d2ede1ccf30602a9c4f82be7a1d43c597f0f5
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-10-25 16:52:13 +0200

    [^] [Example] Worked on test in debug session.

--- examples/me_Delays/me_Delays.ino
+++ examples/me_Delays/me_Delays.ino
@@ -2,7 +2,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2025-09-20
+  Last mod.: 2025-10-25
 */
 
 #include <me_Delays.h>
@@ -11,38 +11,42 @@
 #include <me_Console.h>
 #include <me_Pins.h>
 #include <me_Duration.h>
+#include <me_DebugPrints.h>
+
+const TUint_1 OutputPinNum = 7;
 
 void RunTest()
 {
   /*
-    We're turning on LED for constant amount of time.
-    We're turning off LED for constant amount of time.
+    We're turning ON output pin for constant amount of time.
+    We're turning OFF output pin for constant amount of time.
   */
-  const TUint_1 NumRuns = 3;
+  const TUint_2 Led_TimeOn_Ms = 50;
+  const TUint_2 Led_TimeOff_Ms = 10;
+  const TUint_1 NumRuns = 2;
 
-  const TUint_2
-    Led_TimeOn_Ms = 1000,
-    Led_TimeOff_Ms = 200;
+  me_Pins::TOutputPin OutputPin;
 
-  const TUint_1 Led_Pin = 13;
+  Console.Print("Duration test");
+  Console.Indent();
+  me_DebugPrints::Print("Output pin", OutputPinNum);
+  me_DebugPrints::Print("Time ON (ms)", Led_TimeOn_Ms);
+  me_DebugPrints::Print("Time OFF (ms)", Led_TimeOff_Ms);
+  me_DebugPrints::Print("Number of runs", NumRuns);
 
-  me_Pins::TOutputPin Led;
-
-  Led.Init(Led_Pin);
-  Led.Write(0);
+  OutputPin.Init(OutputPinNum);
+  OutputPin.Write(0);
 
   for (TUint_1 RunNumber = 1; RunNumber <= NumRuns; ++RunNumber)
   {
-    Console.Print(RunNumber);
-    Console.EndLine();
-
-    Led.Write(1);
+    OutputPin.Write(1);
     me_Delays::Delay_Ms(Led_TimeOn_Ms);
-    Led.Write(0);
+    OutputPin.Write(0);
     me_Delays::Delay_Ms(Led_TimeOff_Ms);
   }
 
-  Led.Write(0);
+  Console.Print("Done");
+  Console.Unindent();
 }
 
 /*
@@ -50,34 +54,37 @@ void RunTest()
 */
 void RunInifiniteTest()
 {
-  const TUint_1 Led_Pin = 13;
   const me_Duration::TDuration
-    Led_Wave_Duration = { 0, 0, 0, 160 };
+    On_Duration = { 0, 0, 20, 500 },
+    Off_Duration = { 0, 0, 10, 250 };
 
-  me_Pins::TOutputPin Led;
+  me_Pins::TOutputPin OutputPin;
 
-  Led.Init(Led_Pin);
-  Led.Write(0);
+  Console.Print("Infinite test for oscilloscope");
+
+  OutputPin.Init(OutputPinNum);
+  OutputPin.Write(0);
 
   while(1)
   {
-    Led.Write(1);
-    me_Delays::Delay_Duration(Led_Wave_Duration);
-    Led.Write(0);
-    me_Delays::Delay_Duration(Led_Wave_Duration);
+    OutputPin.Write(1);
+    me_Delays::Delay_Duration(On_Duration);
+    OutputPin.Write(0);
+    me_Delays::Delay_Duration(Off_Duration);
   }
 }
 
 void setup()
 {
   Console.Init();
+  me_Delays::Init();
 
   Console.Print("[me_Delays] test");
-  RunTest();
-  Console.Print("Done");
 
-  Console.Print("Running infinite test for oscilloscope now.");
+  RunTest();
   RunInifiniteTest();
+
+  Console.Print("Done");
 }
 
 void loop()
@@ -87,4 +94,5 @@ void loop()
 /*
   2025-08-21
   2025-09-12
+  2025-10-25
 */
